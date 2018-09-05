e43bb54ba11a30b852d0ebd0a11d2f7e256df505
Julian Rabe <mail@julianrabe.com>
2018-09-05 02:57:21 +0200

    Added tests for ArtNetReceiver

---
+++ lib/src/test/java/de/deltaeight/libartnet/ArtNetReceiverTest.java
@@ -0,0 +1,115 @@
+/*
+ * LibArtNet
+ *
+ * Art-Net(TM) Designed by and Copyright Artistic Licence Holdings Ltd
+ *
+ * Copyright (c) 2018 Julian Rabe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.deltaeight.libartnet;
+
+import de.deltaeight.libartnet.packets.ArtNetPacket;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtNetReceiverTest {
+
+    @Test
+    void constructor() {
+        assertThrows(IllegalArgumentException.class, () -> new ArtNetReceiver(new DatagramSocketMockup(666)));
+    }
+
+    @Test
+    void states() throws SocketException {
+
+        DatagramSocketMockup datagramSocketMockup = new DatagramSocketMockup();
+        ArtNetReceiver artNetReceiver = new ArtNetReceiver(datagramSocketMockup);
+
+        assertSame(ArtNetReceiver.State.Initialized, artNetReceiver.getState());
+        assertThrows(IllegalStateException.class, artNetReceiver::stop);
+
+        artNetReceiver.start();
+
+        assertSame(ArtNetReceiver.State.Running, artNetReceiver.getState());
+        assertThrows(IllegalStateException.class, artNetReceiver::start);
+
+        artNetReceiver.stop();
+
+        assertSame(ArtNetReceiver.State.Stopped, artNetReceiver.getState());
+        assertThrows(IllegalStateException.class, artNetReceiver::start);
+        assertThrows(IllegalStateException.class, artNetReceiver::stop);
+    }
+
+    @Test
+    void exceptionHandler() throws SocketException, InterruptedException {
+
+        AtomicReference<Throwable> handledException = new AtomicReference<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        new ArtNetReceiver(new DatagramSocketMockup(true))
+                .withExceptionHandler(exception -> {
+                    handledException.set(exception);
+                    latch.countDown();
+                })
+                .start();
+
+        latch.await(3, TimeUnit.SECONDS);
+        assertTrue(handledException.get() instanceof IOException);
+    }
+
+    private <T extends ArtNetPacket> void testReceiveHandler(ArtNetReceiverPreparation<T> artNetReceiverPreparation,
+                                                             ArtNetPacketBuilder<T> packetBuilder)
+
+            throws SocketException, InterruptedException {
+
+        AtomicReference<T> packetReference = new AtomicReference<>();
+        CountDownLatch latch = new CountDownLatch(1);
+
+        DatagramSocketMockup datagramSocketMockup = new DatagramSocketMockup();
+
+        artNetReceiverPreparation.prepare(new ArtNetReceiver(datagramSocketMockup), packet -> {
+            packetReference.set(packet);
+            latch.countDown();
+        }).start();
+
+        T packet = packetBuilder.build();
+
+        datagramSocketMockup.injectPacket(packet);
+
+        latch.await(3, TimeUnit.SECONDS);
+        assertEquals(packet, packetReference.get());
+    }
+
+    @Test
+    void receiveHandlers() throws SocketException, InterruptedException {
+        testReceiveHandler(ArtNetReceiver::withArtPollReceiveHandler, new ArtPollBuilder());
+        testReceiveHandler(ArtNetReceiver::withArtPollReplyReceiveHandler, new ArtPollReplyBuilder());
+        testReceiveHandler(ArtNetReceiver::withArtDmxReceiveHandler, new ArtDmxBuilder());
+    }
+
+    @FunctionalInterface
+    private interface ArtNetReceiverPreparation<T extends ArtNetPacket> {
+
+        ArtNetReceiver prepare(ArtNetReceiver artNetReceiver, PacketReceiveHandler<T> packetReceiveHandler);
+    }
+}
---
+++ lib/src/test/java/de/deltaeight/libartnet/DatagramSocketMockup.java
@@ -0,0 +1,87 @@
+/*
+ * LibArtNet
+ *
+ * Art-Net(TM) Designed by and Copyright Artistic Licence Holdings Ltd
+ *
+ * Copyright (c) 2018 Julian Rabe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.deltaeight.libartnet;
+
+import de.deltaeight.libartnet.packets.ArtNetPacket;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+class DatagramSocketMockup extends DatagramSocket {
+
+    private final int localPort;
+    private boolean throwIoException;
+    private ArtNetPacket injectArtNetPacket;
+    private boolean closed;
+
+    DatagramSocketMockup() throws SocketException {
+        this.localPort = 0x1936;
+    }
+
+    DatagramSocketMockup(int localPort) throws SocketException {
+        this.localPort = localPort;
+    }
+
+    DatagramSocketMockup(boolean throwIoException) throws SocketException {
+        this();
+        this.throwIoException = throwIoException;
+    }
+
+    @Override
+    public synchronized void receive(DatagramPacket packet) throws IOException {
+        boolean wait = true;
+        while (!closed && wait) {
+            try {
+                if (throwIoException) {
+                    throwIoException = false;
+                    throw new IOException();
+                } else if (injectArtNetPacket != null) {
+                    packet.setData(injectArtNetPacket.getBytes());
+                    packet.setPort(0x1936);
+                    packet.setLength(injectArtNetPacket.getBytes().length);
+                    injectArtNetPacket = null;
+                    wait = false;
+                }
+                if (wait) {
+                    Thread.sleep(10);
+                }
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+
+    @Override
+    public int getLocalPort() {
+        return localPort;
+    }
+
+    @Override
+    public void close() {
+        closed = true;
+    }
+
+    void injectPacket(ArtNetPacket packet) {
+        injectArtNetPacket = packet;
+    }
+}
