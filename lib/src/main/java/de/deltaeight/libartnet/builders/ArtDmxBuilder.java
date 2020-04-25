b737669bf72fc38b8d5d4f9de9c7c2e80bb71c05
Julian Rabe <julian@deltaeight.de>
2020-04-25 10:46:34 +0200

    Fixed missing  return

--- lib/src/main/java/de/deltaeight/libartnet/builders/ArtDmxBuilder.java
+++ lib/src/main/java/de/deltaeight/libartnet/builders/ArtDmxBuilder.java
@@ -109,7 +109,7 @@ public class ArtDmxBuilder extends ArtNetPacketBuilder<ArtDmx> {
 
             int subUni = packetData[14];
 
-            ArtDmx result = new ArtDmx(packetData[12], packetData[13], packetData[15],
+            return new ArtDmx(packetData[12], packetData[13], packetData[15],
                     (subUni & 0xFF) >>> 4, subUni & 0xF,
                     data.clone(), packetData.clone());
         }
